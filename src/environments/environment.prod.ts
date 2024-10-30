5790849af3f232dd0a304a23b031da801ddcee4f
컴플렉시온 <complexion@keompeullegsion-ui-MacBookPro.local>
2024-10-30 11:13:51 +0900

    add: environments for dev/prod

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyA7BRas0BpsGQPpCMZiNnSXHvjbE5Tn5_s",
+    authDomain: "edicon-dev.firebaseapp.com",
+    databaseURL: "https://edicon-dev.firebaseio.com",
+    projectId: "edicon-dev",
+    storageBucket: "edicon-dev.appspot.com",
+    messagingSenderId: "741585694427",
+    appId: "1:741585694427:web:5fe94dba269889099ba6fe",
+    measurementId: "G-HQGHXK2LQS"
+  }
+  // apiUrl: 'https://api.yourdomain.com',
+  // ... 기타 프로덕션 환경 설정
+};
