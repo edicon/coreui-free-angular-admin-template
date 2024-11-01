a28b6bf57bf1efdb9b70a51360e4cd0b5d450bfc
컴플렉시온 <complexion@keompeullegsion-ui-MacBookPro.local>
2024-11-01 15:09:29 +0900

    test: AngularFire: Databse: CURD

--- src/app/views/pages/fb-login/fb-login.component.ts
+++ src/app/views/pages/fb-login/fb-login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { NgStyle } from '@angular/common';
 import { IconDirective } from '@coreui/icons-angular';
 import {
@@ -15,12 +15,17 @@ import {
   FormControlDirective,
   ButtonDirective,
 } from '@coreui/angular';
-import { Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { Router } from '@angular/router';
 
 import firebase from 'firebase/compat/app';
-import {AngularFireAuth} from '@angular/fire/compat/auth';
-import {firebaseui, FirebaseUIModule} from 'firebaseui-angular';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import {
+  AngularFireDatabase,
+  AngularFireObject,
+} from '@angular/fire/compat/database';
+import { firebaseui, FirebaseUIModule } from 'firebaseui-angular';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-fb-login',
@@ -44,27 +49,46 @@ import {firebaseui, FirebaseUIModule} from 'firebaseui-angular';
     ButtonDirective,
     NgStyle,
     // FirebaseUI
+    // AngularFireDatabaseModule,
     FirebaseUIModule,
   ],
   providers: [],
 })
-export class FbLoginComponent implements OnDestroy {
+export class FbLoginComponent implements OnInit, OnDestroy {
   user: firebase.User | null = null;
   private authSubscription: Subscription | null = null;
 
-  constructor(private afAuth: AngularFireAuth, private router: Router) {
-    this.authSubscription = afAuth.authState.subscribe(this.afAuthChangeListener);
+  constructor(
+    private afAuth: AngularFireAuth,
+    private afDb: AngularFireDatabase,
+    private router: Router,
+  ) {
+    this.authSubscription = afAuth.authState.subscribe(
+      this.afAuthChangeListener,
+    );
   }
+
+  ngOnInit(): void { }
 
   private afAuthChangeListener = (user: firebase.User | null) => {
     // if needed, do a redirect in here
     this.user = user;
     if (user) {
-      console.log('Logged in :)');
+      if (!environment.production) {
+        console.log('Logged in :)');
+        this.testAuthDb();
+      }
       this.router.navigateByUrl('/home/dashboard');
     } else {
       console.log('Logged out :(');
     }
+  };
+
+  testAuthDb(): void {
+    const test$ = this.afDb.object('test');
+    test$.valueChanges().subscribe((data) => {
+      console.log('test data:', data);
+    });
   }
 
   logout(): void {
@@ -77,4 +101,3 @@ export class FbLoginComponent implements OnDestroy {
     }
   }
 }
-
